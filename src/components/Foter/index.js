36194ad1049d3499b1b2bc24413246fece369c9c
Faisal Nour <faisalskhalifa@gmail.com>
2021-04-24 01:44:00 -0400

    links structure has been added to footer

--- src/components/Foter/index.js
+++ src/components/Foter/index.js
@@ -3,9 +3,25 @@ import "./style.css"
 
 function Foter() {
     return (
-        <div className="footer" style={{ textAlign: "center" }}>
-            <div id="copyright" >&#169;2021 Faisal Nour. All Rights Reserved</div>
-        </div>
+        <>
+            <div id="upperFooter" >
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div style={{ textAlign: "center", marginBottom: "15px", marginTop: "15px" }}>
+                                <p>Connect with me through</p>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://www.linkedin.com/in/faisal-nour/" target="_blank" className="button_social"><i id="linkedinlogo" class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i></a></li>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="" target="_blank" className="button_social"><i id="githublogo" class="fab fa-github fa-2x" aria-hidden="true"></i></a></li>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="" target="_blank" className="button_social"><i id="twitterlogo" class="fab fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer" style={{ textAlign: "center" }}>
+                <div id="copyright" >&#169;2021 Faisal Nour. All Rights Reserved</div>
+            </div>
+        </>
     )
 }
 
