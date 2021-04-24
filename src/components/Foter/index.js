6d645be87ca73aab9c330bce2c2eae65f09d7066
Faisal Nour <faisalskhalifa@gmail.com>
2021-04-24 01:57:51 -0400

    updated font style in footer

--- src/components/Foter/index.js
+++ src/components/Foter/index.js
@@ -9,7 +9,7 @@ function Foter() {
                     <div className="row">
                         <div className="col">
                             <div style={{ textAlign: "center", marginBottom: "15px", marginTop: "15px" }}>
-                                <p>Connect with me through</p>
+                                <h6 style={{ marginBottom: "15px" }}>Connect with me through</h6>
                                 <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://www.linkedin.com/in/faisal-nour/" target="_blank" rel="noreferrer" className="button_social"><i id="linkedinlogo" class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i></a></li>
                                 <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://github.com/faisalsnour" target="_blank" rel="noreferrer" className="button_social"><i id="githublogo" class="fab fa-github fa-2x" aria-hidden="true"></i></a></li>
                                 <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="mailto:faisalskhalifa@gmail.com" target="_blank" rel="noreferrer" className="button_social"><i id="twitterlogo" class="fas fa-envelope fa-2x" aria-hidden="true"></i></a></li>
