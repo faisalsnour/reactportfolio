bcae087df59721212c76ff1c3dc29386b71a5b17
Faisal Nour <faisalskhalifa@gmail.com>
2021-04-24 01:50:37 -0400

    email link has been added in footer

--- src/components/Foter/index.js
+++ src/components/Foter/index.js
@@ -10,9 +10,9 @@ function Foter() {
                         <div className="col">
                             <div style={{ textAlign: "center", marginBottom: "15px", marginTop: "15px" }}>
                                 <p>Connect with me through</p>
-                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://www.linkedin.com/in/faisal-nour/" target="_blank" className="button_social"><i id="linkedinlogo" class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i></a></li>
-                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://github.com/faisalsnour" target="_blank" className="button_social"><i id="githublogo" class="fab fa-github fa-2x" aria-hidden="true"></i></a></li>
-                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="" target="_blank" className="button_social"><i id="twitterlogo" class="fab fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://www.linkedin.com/in/faisal-nour/" target="_blank" rel="noreferrer" className="button_social"><i id="linkedinlogo" class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i></a></li>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="https://github.com/faisalsnour" target="_blank" rel="noreferrer" className="button_social"><i id="githublogo" class="fab fa-github fa-2x" aria-hidden="true"></i></a></li>
+                                <li style={{ display: "inline-flex", marginRight: "10px" }}><a href="mailto:faisalskhalifa@gmail.com" target="_blank" rel="noreferrer" className="button_social"><i id="twitterlogo" class="fas fa-envelope fa-2x" aria-hidden="true"></i></a></li>
                             </div>
                         </div>
                     </div>
